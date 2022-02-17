76dd6ac2c34a9ebf1574b9ee5501ad8cb89b6aa9
FabioLucena <joe04_lo@hotmail.com>
2022-02-17 16:48:01 -0300

    endpoint de busca estudante por nome adicionado

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {  criaTurma } from "./endpoints/CriaTurma";
 import { pegaTurmasAtivas } from "./endpoints/pegaTurmasAtivas";
 import { mudarModulo } from "./endpoints/mudarModulo"
 import { criarEstudante } from "./endpoints/criarEstudante"
+import { buscarEstudante } from "./endpoints/buscarEstudante"
 
 app.post("/turma", criaTurma)
 
@@ -11,5 +12,7 @@ app.get("/turma", pegaTurmasAtivas)
 app.put("/turma/modulo", mudarModulo)
 
 // 1645109738351
+
+app.get("/estudante", buscarEstudante)
 
 app.post("/estudante", criarEstudante)
